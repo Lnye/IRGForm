9588623351616f1a971fa640e43469839ae932f4
Luke <39633418+Lnye@users.noreply.github.com>
2018-06-05 12:39:45 -0400

    Update

--- src/app/irgcard/irgcard.component.ts
+++ src/app/irgcard/irgcard.component.ts
@@ -11,20 +11,16 @@ export class IrgcardComponent {
  card = [
   {
   Name:'Form Name', cols: 1, rows: 1,
-  Label: 'Test Button label',
-  imageURL: 'https://png.icons8.com/material/48/00457C/in-inventory.png',
+  Label: 'Capital Control',
+  ButtonURL: 'https://onbase.irgra.com/AppNet/UnityForm.aspx?d1=AcdhaemfAW%2f0l5QkfqMYFgijoO%2fTAHax63%2b8YaRKqaAhAHmYJKgVAMnoeZIl3n3NdH%2bGzj1Wh2iyhBVwOVeS7SAdIFuwHxvVlliRt%2fe35A1Fo%2bbaMisXg%2bkYY6s7snEvR1qnDOOk01sOenQQDFMk4gorK8sASq9ZL49dJAhJbJjjYYobs%2b7jQmNw8qULQoj6VYcYMgkFI%2bmhCQgMMVYodJo%3d',
+  imageURL: 'https://png.icons8.com/material/512/00457C/cheap-2.png',
   imageDesc: 'Image Description',
 },
 {
   Name:'Form Name', cols: 1, rows: 1,
-  Label: 'Test Button label 2',
-  imageURL: 'https://png.icons8.com/material/48/00457C/in-inventory.png',
-  imageDesc: 'Image Description',
-  },
-{
-  Name:'Form Name', cols: 1, rows: 1,
-  Label: 'Test Button label 3',
-  imageURL: 'https://png.icons8.com/material/48/00457C/in-inventory.png',
+  Label: 'IT Project Request',
+  ButtonURL: 'https://www.wrike.com/form/eyJhY2NvdW50SWQiOjE5MjQzNDMsInRhc2tGb3JtSWQiOjEwMjk4NX0JNDY4MDA5NjMwNjU1MgkxYjlhNzEyZGZhMTg4NDZhYWJlNjU5NTUzZjYwZDZhYTdkZWUxMDRiMzc5ZTllYjBlOGEyMDRkMWMzOWQ1MGU3',
+  imageURL: 'https://png.icons8.com/material/480/00457C/serial-tasks.png',
   imageDesc: 'Image Description',
   },
 ];
